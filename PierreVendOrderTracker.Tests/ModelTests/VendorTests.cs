45ceb58c89e4adb664f32cbfa9baf3b218105875
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-03-03 13:54:45 -0800

    add vendorTests class

---
+++ PierreVendOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System;
+
+namespace PierreVendOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests 
+  {
+  }
+}
