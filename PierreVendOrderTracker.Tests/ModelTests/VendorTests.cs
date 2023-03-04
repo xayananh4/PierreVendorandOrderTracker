cf5b89fc802d69e65222beb16f550a9b2baf18c9
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-03-03 17:06:23 -0800

    test: GetId_ReturnsVendorId_Int(), good pass

--- PierreVendOrderTracker.Tests/ModelTests/VendorTests.cs
+++ PierreVendOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -6,8 +6,12 @@ using System;
 namespace PierreVendOrderTracker.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
