06f56bc8eb87001dd903a1a7bde06d047567414f
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-03-04 15:12:58 -0800

    GetTitle_ReturnsTitle_String(), good pass

--- PierreVendOrderTracker.Tests/ModelTests/OrderTest.cs
+++ PierreVendOrderTracker.Tests/ModelTests/OrderTest.cs
@@ -16,5 +16,19 @@ namespace PierreVendOrderTracker.Tests
 
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetTitle_ReturnsTitle_String()
+    {
+      //Arrange
+      string Title = "bread";
+      
+      DateTime dt1 = new DateTime();
+      Order newOrder = new Order("bread", "desc", 1, dt1);
+      string result = newOrder.Title;
+
+      //Assert
+      Assert.AreEqual(Title, result);
+    }
   }
 }
