a8c469126126b71409d62e5fde938912030aad5f
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-03-03 20:58:21 -0800

    test: GetAll_ReturnsAllVendorObjects_VendorList(), good pass

--- PierreVendOrderTracker/Models/Order.cs
+++ PierreVendOrderTracker/Models/Order.cs
@@ -1,13 +1,14 @@
 using System.Collections.Generic;
+using System;
 
 namespace PierreVendOrderTracker.Models
 {
   public class Order
   {
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public int Id { get; }
-    public int Price { get; }
-    DateTime date = new DateTime(); 
+    // public string Title { get; set; }
+    // public string Description { get; set; }
+    // public int Id { get; }
+    // public int Price { get; }
+    // DateTime date = new DateTime(); 
   }
 }
--- PierreVendOrderTracker/Models/Vendor.cs
+++ PierreVendOrderTracker/Models/Vendor.cs
@@ -10,5 +10,23 @@ namespace PierreVendOrderTracker.Models
     public string Name { get; set; }
     public string Description { get; set; }
     public int Id { get; }
+
+    public Vendor(string name, string discription)
+    {
+      Name = name;
+      Description = discription;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
   }
 }
