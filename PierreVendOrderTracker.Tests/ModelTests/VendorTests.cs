18ad7ca87ebb760f081dc83029b3746ba8921868
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-03-03 20:56:57 -0800

    test: GetAll_ReturnsAllVendorObjects_VendorList(), good fail

--- PierreVendOrderTracker.Tests/ModelTests/VendorTests.cs
+++ PierreVendOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -61,5 +61,24 @@ namespace PierreVendOrderTracker.Tests
       //Assert
       Assert.AreEqual(1, result);
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      //Arrange
+      string name01 = "Work";
+      string name02 = "School";
+      string desc01 = "Work";
+      string desc02 = "School";
+      Vendor newVendor1 = new Vendor(name01, desc01);
+      Vendor newVendor2 = new Vendor(name02, desc02);
+      List<Vendor> newListOfVendors = new List<Vendor> { newVendor1, newVendor2 };
+
+      //Act
+      List<Vendor> result = Vendor.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newListOfVendors, result);
+    }
   }
 }
