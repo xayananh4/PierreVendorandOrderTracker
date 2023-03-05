15a2decde25a8431141257f9be0e58ac8c9ecc44
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-03-04 19:18:17 -0800

    WIP- allow user to add new order to a vendor

--- PierreVendOrderTracker/Controllers/VendorController.cs
+++ PierreVendOrderTracker/Controllers/VendorController.cs
@@ -14,20 +14,49 @@ namespace PierreVendOrderTracker.Controllers
       return View(allVendors);
     }
 
-    //Comes here after submitting a new vendor in the New Form 
-    [HttpPost("/vendors")]
-    public ActionResult Create(string vendorName, string discription)
-    {
-      Vendor newvendor = new Vendor(vendorName, discription);
-      return RedirectToAction("Index");
-    }
-
     //Comes here after clicking link to add a new vendor in View/Vendor/index.cshtml
     [HttpGet("/vendors/new")]
     public ActionResult New()
     {
       return View();
     }
+
+    //Comes here after submitting a new vendor in the New Form 
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string discription)
+    {
+      Vendor newvendor = new Vendor(vendorName, discription);
+      return RedirectToAction("Index", "Home");
+    }
+
+    //Comes here after clicking on vendor link in the index page - clicking on a vendor
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> VendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", VendorOrders);
+      return View(model);
+    }
+
+    // This one creates new order within a Vendor 
+
+    [HttpPost("/categories/{categoryId}/items")]
+    public ActionResult Create(int categoryId, string itemDescription)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category foundCategory = Category.Find(categoryId);
+      Item newItem = new Item(itemDescription);
+      foundCategory.AddItem(newItem);
+      List<Item> categoryItems = foundCategory.Items;
+      model.Add("items", categoryItems);
+      model.Add("category", foundCategory);
+      return View("Show", model);
+    }
+
+
 
   }
 }
--- PierreVendOrderTracker/Models/Vendor.cs
+++ PierreVendOrderTracker/Models/Vendor.cs
@@ -17,6 +17,7 @@ namespace PierreVendOrderTracker.Models
       Description = discription;
       _instances.Add(this);
       Id = _instances.Count;
+      Orders = new List<Order> { };
     }
 
     public static void ClearAll()
@@ -31,6 +32,11 @@ namespace PierreVendOrderTracker.Models
     public static Vendor Find(int searchId)
     {
       return _instances[searchId - 1];
+    }
+
+    public void AddOrder(Order item)
+    {
+      Orders.Add(item);
     }
 
 
