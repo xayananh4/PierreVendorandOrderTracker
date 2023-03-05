cd2af8d77ba5f98e42a8551f038fe09ed88c17bb
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-03-04 18:17:03 -0800

    add Home, and Vendor controller.  Made it possible for user to add a new vendor using a form

--- PierreVendOrderTracker/Controllers/VendorController.cs
+++ PierreVendOrderTracker/Controllers/VendorController.cs
@@ -1,15 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+using PierreVendOrderTracker.Models;
+using System.Collections.Generic;
 
-namespace ToDoList.Controllers
+namespace PierreVendOrderTracker.Controllers
 {
-    public class VendorController : Controller
+  public class VendorController : Controller
+  {
+
+    [HttpGet("/vendors")]
+    public ActionResult Index()
     {
-
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
     }
+
+    //Comes here after submitting a new vendor in the New Form 
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string discription)
+    {
+      Vendor newvendor = new Vendor(vendorName, discription);
+      return RedirectToAction("Index");
+    }
+
+    //Comes here after clicking link to add a new vendor in View/Vendor/index.cshtml
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+  }
 }
